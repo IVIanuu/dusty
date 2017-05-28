48c2aa946d7897b08d43013860beee3e7722fb3e
IVIanuu <IVIanuu@gmail.com>
2017-05-29 01:49:17 +0200

    Hopefully fixed gradle 4.0

--- annotations/src/main/java/com/ivianuu/dusty/annotations/Clear.java
+++ annotations/src/main/java/com/ivianuu/dusty/annotations/Clear.java
@@ -5,9 +5,5 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * @author Manuel Wrage (IVIanuu)
- */
-
 @Retention(RetentionPolicy.CLASS)@Target(ElementType.FIELD)
 public @interface Clear {}
